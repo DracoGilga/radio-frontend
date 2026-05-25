00159f8ec3c031b5285f595ad5caa6d060eab00d
DracoGilga <cesar96707@gmail.com>
2026-05-25 16:31:37 -0600

    feat: add redirects to Vercel configuration for improved routing

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,10 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-	plugins: [vue()],
-	server: {
-		headers: {
-			'Content-Security-Policy': "script-src 'self' 'unsafe-eval' 'unsafe-inline';"
-		}
-	},
-	base: './',
+    plugins: [vue()],
+    server: {
+        headers: {
+            'Content-Security-Policy': "script-src 'self' 'unsafe-eval' 'unsafe-inline';"
+        }
+    }
 })
